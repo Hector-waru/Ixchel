8b1e07383bda24a98a897c3cd32b4b6a5f949d90
Hector Garcia Ortiz <Hector.Garcia.Ortiz@gmail.com>
2022-02-15 20:26:42 -0600

    artesanos.html nuevo archivo:  assets/css/alebrije-responsivo.css modificado:     assets/css/carSh.css modificado:     assets/css/footer.css modificado:     assets/css/joyeria.css modificado:     assets/css/navbar.css modificado:     assets/css/principal.css nuevo archivo:  assets/js/complementos.js modificado:     carrito.html modificado:     catalogo-arte-alebrijes.html modificado:     index.html
    15-02-22

---
+++ assets/js/complementos.js
@@ -0,0 +1,33 @@
+document.getElementById("navBar").innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark">
+<div class="container-fluid">
+  <a class="navbar-brand" href="index.html"><img src="imagenes/logo.png" width="150"></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav me-5 mb-2 mb-lg-0">
+      <li class="nav-item left-bar artesanos">
+        <a class="nav-link active" aria-current="page" href="artesanos.html">Artesanos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="catalogo.html">Catalogo</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="niciar-sesion.html">Iniciar Sesion</a>
+      </li>
+      <li class="nav-item signup">
+        <button class="signup-btn">
+        <a class="nav-link active" href="registro-comprador.html">Regístrate</a>
+        </button>
+      </li>
+        <li>
+          <a class="navbar-brand" href="carrito.html"><img src="imagenes/cart.png" width="30"></a>
+        </li>
+      
+        
+    
+    </ul>
+  </div>
+</div>
+</nav>`
