36b8f76ad374756a97b1978142bd00057e38c8b9
Rebeca Cuevas <rebecaacuevasa@gmail.com>
2022-02-17 09:02:02 -0600

    páginas responsive, pedido, publicar producto

--- assets/js/publicar-producto.js
+++ assets/js/publicar-producto.js
@@ -8,3 +8,11 @@ function mostrarJoyeria() {
     document.getElementById('arte').style.display = 'none';
     document.getElementById('joyeria').style.display = 'block';
 }
+
+function ocultarInventario() {
+    document.getElementById('stock').style.display = 'none';
+}
+
+function ingresarInventario() {
+    document.getElementById('stock').style.display = 'block';
+}
